cef677676dae93579768231338d25f0dfb4daebd
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-05 18:06:20 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-# methind for input of a name
+# method for input of a name
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
 end
@@ -11,7 +11,6 @@ end
 
 def tip_calculator(price, tip = 0.2)
   tip_amount = price * tip
-  
   total = price + tip_amount
   
   puts "The tip amount is #{tip_amount} and the total is #{total}"
