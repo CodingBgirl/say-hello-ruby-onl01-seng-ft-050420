704e6736a57461cb9021f01933f3642145709ded
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-05 18:07:06 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -8,14 +8,3 @@ end
 # say_hello("Ibiene") --> "Hello Ibiene"
 # and 
 # say_hello() --> "Hello Ruby Programmer"
-
-def tip_calculator(price, tip = 0.2)
-  tip_amount = price * tip
-  total = price + tip_amount
-  
-  puts "The tip amount is #{tip_amount} and the total is #{total}"
-end
-
-## test will run this
-tip_calculator(60, 0.15)
-tip_calculator(60)
