da39b3a21e22c3ae69165787ba4d69d69cf0ccf3
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-05 17:45:20 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,7 @@
 # Build your say_hello method here
 # methind for input of a name
-#def say_hello(name)
-  #puts "Hello #{name}!"
-#end
-
-def say_hello()
-  puts "Hello Ruby Programmer!"
+def say_hello(name)
+  puts "Hello #{name}!"
 end
+
 
