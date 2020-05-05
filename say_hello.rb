c0bab95f524fa8842e8dd705575492e329ce5deb
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-05 18:07:12 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+# method for input of a name
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+# To pass test it needs to be
+# say_hello("Ibiene") --> "Hello Ibiene"
+# and 
+# say_hello() --> "Hello Ruby Programmer"
